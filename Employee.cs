68e3f46ef3b76095dc9a709fcf4567f01ad09d12
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-01-24 15:28:15 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment12
+{
+    public class Employee
+    {
+        protected int id;
+        protected string name;
+        protected double basic, hra, da, ta, gross, pf;
+        private static int count;
+        public Employee(string name, double basic)
+        {
+            count++;
+            id = count;
+            this.name = name;
+            this.basic = basic;
+        }
+
+        public virtual void CalculateSalary()
+        {
+            hra = basic * 0.40;
+            da = basic * 0.20;
+            ta = basic * 0.10;
+            pf = basic * 0.12;
+            gross = (basic + hra + da + ta) - pf;
+        }
+        public string EmployeeDetails()
+        {
+            return $"\n Id ={id} \n Name ={name} \n Basic Salary ={basic} \n Gross of Employee ={gross}";
+        }
+    }
+
+    public class Manager : Employee
+    {
+        private double food, ma;
+        // base keyword will call the base class constructor
+        public Manager(string name, double basic) : base(name, basic)
+        {
+
+        }
+        public override void CalculateSalary()
+        {
+            hra = basic * 0.40;
+            da = basic * 0.20;
+            ta = basic * 0.10;
+            pf = basic * 0.12;
+            food = basic * 0.05;
+            ma = basic * 0.12;
+            gross = (basic + hra + da + ta + food) - (pf + ma);
+
+        }
+        public string DisplayManagerDetails()
+        {
+            return $"\n Id ={id} \n Name ={name} \n Basic Salary ={basic} \n Gross of Manager ={gross}";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m1 = new Manager("Kishor", 41000);
+            m1.CalculateSalary();
+            Console.WriteLine(m1.DisplayManagerDetails());
+
+
+            Employee e1 = new Employee("Tushar", 27000);
+            e1.CalculateSalary();
+            Console.WriteLine(e1.EmployeeDetails());
+
+        }
+
+    }
+}
